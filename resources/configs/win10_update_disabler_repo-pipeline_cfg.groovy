5be175b70ab66802064c5cd0f87c3cd87e7dbcde
Serhii5465 <serhiiartiukh5@gmail.com>
2024-06-13 12:00:22 +0300

    win10_update_disabler_repo-pipeline_cfg - Added a function

--- resources/configs/win10_update_disabler_repo-pipeline_cfg.groovy
+++ resources/configs/win10_update_disabler_repo-pipeline_cfg.groovy
@@ -1,7 +1,9 @@
-return [
-    git_repo_url: 'win10_update_disabler_script_ps_repo:Serhii5465/win10_update_disabler_script-ps.git',
-    git_branch: 'main'
-    stash_includes: 'upd_disabler.ps1',
-    stash_excludes: '',
-    command: 'robocopy . D:\\system\\Disabler_Win10_Updates upd_disabler.ps1'
-]
+def call(){
+    return [
+        git_repo_url: 'win10_update_disabler_script_ps_repo:Serhii5465/win10_update_disabler_script-ps.git',
+        git_branch: 'main'
+        stash_includes: 'upd_disabler.ps1',
+        stash_excludes: '',
+        command: 'robocopy . D:\\system\\Disabler_Win10_Updates upd_disabler.ps1'
+    ]
+}
