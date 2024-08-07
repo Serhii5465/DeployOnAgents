e5fce72628c009b183cc5328cd3ef42a25ea53e1
Serhii5465 <serhiiartiukh5@gmail.com>
2024-08-07 16:02:29 +0300

    UnstashOnAgent - Selection of deployment command based on OS

--- vars/UnstashOnAgent.groovy
+++ vars/UnstashOnAgent.groovy
@@ -1,12 +1,16 @@
-def call(label, command_deploy, func_deploy){
+def call(platform, label, command_deploy, func_deploy){
     return {
         node(label){
             stage('Unstash on ' + label){
                 script{
                     unstash 'src'
 
-                    if(command_deploy){
+                    if (command_deploy && platform == 'win32'){
                         bat returnStatus: true, script: command_deploy
+                    }
+
+                    if(command_deploy && platform == 'linux'){
+                        sh returnStatus: true, script: command_deploy
                     }
 
                     if(func_deploy){
